0735a1a2c4055f4fa8135d3f367107537c6751c9
Breno Goncalves Renzi Del Cacho <2403703@SALAS.AULAS>
2025-03-07 10:42:22 -0300

    feat: adding new pages amd new styles

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const pesquisarInput = document.getElementById("principal__Input");
 
+const mensagensDeErro = ["SEU OTARO, ESCREVE CORRETO", "OLHA O CAMINHAR DO ELEMENTO< È UM PASSO BEM LENTO, VAI FECHANDO O TEMPO", "IRMÂO, SE N SABE CLICA NOS BOTÕES LI ANIMAL", "BRENO IS COLEGIAL", "AAAAAAAAAAAAAAAAAAA"]
+
 pesquisarInput.addEventListener("keydown", (event) => {
   const valorDaPesquisa = pesquisarInput.value;
   pesquisar(event, valorDaPesquisa);
@@ -7,16 +9,17 @@ pesquisarInput.addEventListener("keydown", (event) => {
 
 function pesquisar(event, valor) {
   if (event.key == "Enter") {
-    switch (valor.toLowerCase()) {
-      case "ezequiel" || "ezequiel magoga" || "mano z":
-        window.location.href = "../pages/ezequiel-inicio.html";
-        break;
-      case "breno" || "breno Gonçalves" || "colegial":
-        window.location.href = "../pages/breno-inicio.html";
-        break;
-      default:
-        window.location.href = "../pages/errodepesquisar.html";
-        break;
+    if (valor.toLowerCase() == "ezequiel" || valor.toLowerCase() == "ezequiel magoga" || valor.toLowerCase() == "mano z") {
+      window.location.href = "../pages/ezequiel-inicio.html";
+    } else if (valor.toLowerCase() == "breno" || valor.toLowerCase() == "breno gonçalves" || valor.toLowerCase() == "olha o caminhar do elemento, é um passo bem lento, vai fechando o tempo") {
+      window.location.href = "../pages/breno-inicio.html";
+    } else {
+      mostrarMensagemDeErro()
     }
   }
+}
+
+function mostrarMensagemDeErro() {
+  var indiceAleatorio = Math.floor(Math.random() * mensagensDeErro.length);
+  alert(mensagensDeErro[indiceAleatorio])
 }
