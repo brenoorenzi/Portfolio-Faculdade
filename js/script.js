022c87beb3cd05083c2bdc4b904b1d0c549e5894
Breno Goncalves Renzi Del Cacho <2403703@SALAS.AULAS>
2025-03-14 11:06:46 -0300

    style: adding styles to pages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const pesquisarInput = document.getElementById("principal__Input");
 
-const mensagensDeErro = ["SEU OTARO, ESCREVE CORRETO", "OLHA O CAMINHAR DO ELEMENTO< È UM PASSO BEM LENTO, VAI FECHANDO O TEMPO", "IRMÂO, SE N SABE CLICA NOS BOTÕES LI ANIMAL", "BRENO IS COLEGIAL", "AAAAAAAAAAAAAAAAAAA"]
+const mensagensDeErro = ["ERRO 404"]
 
 pesquisarInput.addEventListener("keydown", (event) => {
   const valorDaPesquisa = pesquisarInput.value;
@@ -9,9 +9,9 @@ pesquisarInput.addEventListener("keydown", (event) => {
 
 function pesquisar(event, valor) {
   if (event.key == "Enter") {
-    if (valor.toLowerCase() == "ezequiel" || valor.toLowerCase() == "ezequiel magoga" || valor.toLowerCase() == "mano z") {
+    if (valor.toLowerCase().trim() == "ezequiel" || valor.toLowerCase().trim() == "ezequiel magoga" || valor.toLowerCase().trim() == "mano z") {    
       window.location.href = "../pages/ezequiel-inicio.html";
-    } else if (valor.toLowerCase() == "breno" || valor.toLowerCase() == "breno gonçalves" || valor.toLowerCase() == "olha o caminhar do elemento, é um passo bem lento, vai fechando o tempo") {
+    } else if (valor.toLowerCase().trim() == "breno" || valor.toLowerCase().trim()== "breno gonçalves" || valor.toLowerCase().trim() == "olha o caminhar do elemento, é um passo bem lento, vai fechando o tempo") {
       window.location.href = "../pages/breno-inicio.html";
     } else {
       mostrarMensagemDeErro()
@@ -23,3 +23,4 @@ function mostrarMensagemDeErro() {
   var indiceAleatorio = Math.floor(Math.random() * mensagensDeErro.length);
   alert(mensagensDeErro[indiceAleatorio])
 }
+
